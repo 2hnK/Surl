5365c1fd0d301c8aa5de617a9fa771a6533cb25c
2hnK <kimgh921@gmail.com>
2024-07-20 20:52:21 +0900

    20240720

--- src/main/java/com/ll/demo_02/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo_02/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "2hnK's" + siteName;
+        return "2hnK's " + siteName;
     }
 
     @GetMapping("/secret")
